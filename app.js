f8f6de6e866df06d8c30bed544d21c7bed72d787
AGarciaNY <aldairgarcia360@gmail.com>
2023-02-05 18:14:57 -0500

    test $

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-// const Koa = require('koa');
-// const app = new Koa();
-
 const express = require('express');
 const app = new express();
-
-// const port = 5000; 
-// const cors = require('cors');
-// // const pool = require('./db.js');
-
-// app.use(express.json());
-// app.use(cors());
-// app.use(async ctx => {
-//     ctx.body = 'Hello World aaa';
-// });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
+app.get('/here_is_me', (req, res) => {
+    res.send('Hello World! TWO');
+});
 app.listen(5000);
