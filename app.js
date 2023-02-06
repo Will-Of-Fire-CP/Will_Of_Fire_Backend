de5bb944f773b0812ef4c18775345e3ea0ab8334
AGarciaNY <aldairgarcia360@gmail.com>
2023-02-06 15:45:36 -0500

    test 12

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require("./db");
+// const pool = require("./db");
 
 const app = new express();
 const friendRequestsRouter = require("./routes/friendRequestsRouter.js")
@@ -16,5 +16,10 @@ app.get('/here_is_me', (req, res) => {
     res.send('Hello World! TWO');
 });
 
+app.get('/one', (req, res) => {
+    res.send('test Hello World! TWO');
+});
+
+
 app.use('/firend_requests', friendRequestsRouter)
-app.listen(5000);
+app.listen(3000);
