4194b17ed5533dffe703f97b6f7515eebac4ffa1
AGarciaNY <aldairgarcia360@gmail.com>
2023-02-21 14:50:03 -0500

    fix: raiway user's id was 'user_id' not 'id

--- models/friendModel.js
+++ models/friendModel.js
@@ -1,7 +1,7 @@
 const pool = require("../db.js");
 
 const getFriendsByID = async (user_id) => {
-    const data = await pool.query("SELECT friend_id, user_name, user_score FROM (SELECT * FROM friends WHERE user_id = $1) AS friendstwo JOIN users ON users.id = friendstwo.friend_id", [user_id]).then(result => { return result.rows });
+    const data = await pool.query("SELECT friend_id, user_name, user_score FROM (SELECT * FROM friends WHERE user_id = $1) AS friendstwo JOIN users ON users.user_id = friendstwo.friend_id", [user_id]).then(result => { return result.rows });
     return data
 }
 
