00eb86750860cdf886d7e2e40f3fe3798ed8600c
AGarciaNY <aldairgarcia360@gmail.com>
2023-02-21 14:30:13 -0500

    bug: get friends returns err when trying to get the user's friends

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -4,6 +4,7 @@ const getFriendByID = async (req, res) => {
     try {
         console.log(req.params.id)
         let data = await friendModel.getFriendsByID(req.params.id)
+        console.log(data)
         res.send(data);
 
     } catch (err) {
